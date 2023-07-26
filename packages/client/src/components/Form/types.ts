257cdd5335467bd618f6378b9d13332df4d5d2ea
toropovofficial <toropovofficial@yandex.ru>
2023-07-26 17:28:47 +0300

    Добавил страницу логина

--- packages/client/src/components/Form/types.ts
+++ packages/client/src/components/Form/types.ts
@@ -2,6 +2,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import React from 'react'
 
 export interface CustomFormProps {
+  className: string
   onSubmit: SubmitHandler<any>
   methods: ReturnType<typeof useForm>
   className?: string,
