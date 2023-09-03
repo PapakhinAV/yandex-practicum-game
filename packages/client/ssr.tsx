be8cb57572049738cb42be55ba9537f3db2f5253
Papakhin Alexander <alexandr.papakhin@poleinvest.ru>
2023-09-03 16:50:21 +0300

    Правки ревью v2

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,17 +1,33 @@
-
-// @ts-ignore
-import React from 'react'
+import * as React from 'react'
 import App from './src/App/App'
 import { renderToString } from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router-dom/server'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './src/store/reducer'
+import { CacheProvider } from '@emotion/react'
 
-export function render(store, url) {
-  return renderToString(
-    <Provider store={store}>
-      <StaticRouter location={url}>
-        <App />
-      </StaticRouter>
-    </Provider>
+export function render(store, url, cache) {
+
+  return renderToString (
+    <CacheProvider value={cache}>
+      <Provider store={store}>
+        <StaticRouter location={url}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    </CacheProvider>
   )
 }
+
+export function createStore(preloadedState: any) {
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState: preloadedState
+  })
+
+  return store
+}
+
+
+
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -4,6 +4,10 @@ import express from 'express'
 import * as fs from 'fs'
 import * as path from 'path'
 import { createServer as createViteServer } from 'vite'
+import type { ViteDevServer } from 'vite'
+import type { EmotionCache } from '@emotion/css'
+import createEmotionServer from '@emotion/server/create-instance'
+import createCache from '@emotion/cache'
 
 dotenv.config()
 
@@ -11,24 +15,26 @@ const isDev = () => process.env.NODE_ENV === 'development'
 
 async function startServer (){
 
-  // createClientAndConnect()
-
   const app = express()
 
   app.use(cors())
 
+  let vite: ViteDevServer | undefined
   const port = Number(process.env.SERVER_PORT) || 3001
   const distPath = path.dirname(require.resolve('client/dist/index.html'))
   const srcPath = path.dirname(require.resolve('client'))
-  const storeHelpersPath = require.resolve('client/src/store/helpers.ts')
+  const ssrClientPath = require.resolve('client/dist-ssr/client.cjs')
 
-   const vite = await createViteServer({
+
+  if (isDev()) {
+    vite = await createViteServer({
       server: { middlewareMode: true },
       root: srcPath,
       appType: 'custom'
     })
 
     app.use(vite.middlewares)
+  }
 
 
   app.get('/api', (_, res) => {
@@ -45,24 +51,55 @@ async function startServer (){
     try {
       let template: string
 
-      template = fs.readFileSync(path.resolve(srcPath, 'index.html'), 'utf-8',)
-      template = await vite!.transformIndexHtml(url, template)
+      if (!isDev()) {
+        template = fs.readFileSync(
+          path.resolve(distPath, 'index.html'),
+          'utf-8',
+        )
+      } else {
+        template = fs.readFileSync(
+          path.resolve(srcPath, 'index.html'),
+          'utf-8',
+        )
 
-      const render = (await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx'))).render
+        template = await vite!.transformIndexHtml(url, template)
 
-      const createStore = (await vite!.ssrLoadModule(storeHelpersPath)).createStore
+      }
 
-      const configureStore = await createStore({}) // Инициируется initialStore
+      let render: (store: any, ulr: string, cache:  EmotionCache) => Promise<string>
+      let createStore: (initialStore: any) => Promise<any>
 
-      const appHtml = await render(configureStore, url)
+      if (!isDev()) {
+        const client = await import(ssrClientPath)
+        render = client.render
+        createStore = client.createStore
 
-      const state = configureStore?.getState()
+      } else {
+        const client = await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx'))
+        render = client.render
+        createStore = client.createStore
+      }
+
+
+      const storeInstance = await createStore({}) // Инициируется initialStore
+
+      const cacheKey = 'custom'
+      const cache = createCache({ key: cacheKey })
+      const { extractCritical } = createEmotionServer(cache)
+
+      const appHtml = await render(storeInstance, url, cache)
+
+      const { html, css, ids } = extractCritical(appHtml) // Извлечение стилей и идентификаторов
+
+      const state = storeInstance?.getState()
 
       const stateMarkup = `<script>window.___REDUX_STATE___=${JSON.stringify(state)}</script>`
 
-      const html = template.replace('<!--ssr-outlet-->', stateMarkup + appHtml)
+      const finalHtml = template
+        .replace('<!--ssr-outlet-->', stateMarkup + html)
+        .replace('</head>', `<style data-emotion="${cacheKey} ${ids.join(' ')}">${css}</style></head>`)
 
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(finalHtml)
     } catch (e) {
       if (isDev()) {
         vite!.ssrFixStacktrace(e as Error)
