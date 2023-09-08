02aa43263aaf0a8d1c75116a175965599e4dbc04
Dmitriy <dmi3y.kochetkov@gmail.com>
2023-09-08 20:31:57 +0300

    YAN-37 Написать тесты на UI (#32)
    * YAN-37 Тесты на UI

---
+++ packages/client/src/pages/Login/Login.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import Login from './Login'
+import '@testing-library/jest-dom'
+import { Provider } from 'react-redux'
+import store from '../../store/store'
+
+const title = 'Вход'
+
+describe('Компонент Login', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve('hey'),
+      })
+    )
+  })
+
+  const defaultStore = { ...store }
+  test('рендерится', () => {
+    render(
+      <Provider store={defaultStore}>
+        <MemoryRouter>
+          <Login/>
+        </MemoryRouter>
+      </Provider>
+    )
+
+    const loginPageTitle = screen.getByText(title)
+    expect(loginPageTitle).toBeInTheDocument()
+  })
+})
