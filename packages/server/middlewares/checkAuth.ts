b4fe291f4bdee1fb19516fbd013e733a6c261ad9
kirill.hodyrev <kirill.hodyrev@ramax.ru>
2023-09-13 09:25:40 +0300

    Реализовать API для форума

--- packages/server/middlewares/checkAuth.ts
+++ packages/server/middlewares/checkAuth.ts
@@ -2,7 +2,6 @@ import type { RequestHandler } from 'express'
 
 
 export const checkAuth: RequestHandler = async (req, res, next) => {
-  console.log(req.headers.cookie)
   if (req.headers.cookie) {
     fetch('https://ya-praktikum.tech/api/v2/auth/user', {
       headers: {
