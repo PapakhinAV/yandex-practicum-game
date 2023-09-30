ede5ceb0a1a79eae012b19604e1ea076e96a1306
Alexander Papakhin <71031827+PapakhinAV@users.noreply.github.com>
2023-09-30 23:02:35 +0700

    YAN-66.1 Убрал дублирующее экранирование (#45)
    Co-authored-by: Papakhin Alexander <alexandr.papakhin@poleinvest.ru>

--- packages/client/src/components/fields/Input/Input.tsx
+++ packages/client/src/components/fields/Input/Input.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import { Input } from '@chakra-ui/react'
 import { CustomInputProps } from './types'
-import he from 'he'
 
 
-const CustomInput: React.FC<CustomInputProps> = ({ className, register, value = '', ...props }) => {
+const CustomInput: React.FC<CustomInputProps> = ({ className, register, ...props }) => {
 
-  // React автоматически экранирует специальные символы HTML, такие как <, >, &,
-  // чтобы они были отображены как обычный текст, а не интерпретировались как часть HTML-разметки.
-  // Например, если пользователь вводит <script>alert("XSS")</script> в поле ввода,
-  // React преобразует его в &lt;script&gt;alert("XSS")&lt;/script&gt;
-  //
-  // Однако, в рамках задачи YAN-61 добавил явное экранирование вводимых данных.
-
-  const sanitizedValue = value ? he.encode(String(value)) : ''
-
-  return <Input bg={props.backgroundColor || 'white'} {...props} {...register} value={sanitizedValue} className={className} />
+  return <Input bg={props.backgroundColor || 'white'} {...props} {...register} className={className} />
 }
 
 export default CustomInput
