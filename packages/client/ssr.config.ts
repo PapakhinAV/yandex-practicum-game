08e40268b115f6e9fd72c7b93d970ae41e35f393
Dmitriy <dmi3y.kochetkov@gmail.com>
2023-10-08 15:29:51 +0300

    YAN-66.6 fix CSP for HTTPS (#52)
    +

--- packages/client/ssr.config.ts
+++ packages/client/ssr.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   define: {
     __SERVER_PORT__: Number(process.env.SERVER_PORT) || 3000,
-    __SERVER_API__: `'${process.env.SERVER_API}'` || '',
+    __SERVER_API__: `'${process.env.SERVER_API}'` || 'https://4tunas-ork-rush-28.ya-praktikum.tech',
   },
   plugins: [react(),
     VitePWA({
